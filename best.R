183104cdde6ad9538d3f5dc05d0e14730fd457c7
kumarshaket <kumar.shaket@gmail.com>
2020-07-12 15:56:12 +0530

    pushing R Assignment 4

---
+++ best.R
@@ -0,0 +1,28 @@
+best<- function(state, outcome)
+{
+        outcome1 <- read.csv("outcome-of-care-measures.csv",
+                             colClasses = "character")
+        
+        as.numeric(gsub(",","",outcome1,fixed=TRUE))
+        if(!any(state == outcome1$State)){
+                stop("invalid state")}
+        else if((outcome %in% c("heart attack", "heart failure",
+                                "pneumonia")) == FALSE) {
+                stop(print("invalid outcome"))
+        }
+        outcome2 <- subset(outcome1, State == state)
+        if (outcome == "heart attack") {
+                colnum <- 11
+        }
+        else if (outcome == "heart failure") {
+                colnum <- 17
+        }
+        else {
+                colnum <- 23
+        }
+        min_row <- which(as.numeric(outcome2[ ,colnum]) == 
+                                 min(as.numeric(outcome2[ ,colnum]), na.rm = TRUE))
+        hospitals <- outcome2[min_row,2]
+        hospitals <- sort(hospitals)
+        return(hospitals[1])
+}
---
+++ rankhospital.R
@@ -0,0 +1,32 @@
+rankhospital<- function(state, outcome, num = "best")
+{
+        outcome1 <- read.csv("outcome-of-care-measures.csv",
+                             colClasses = "character")
+        as.numeric(gsub(",","",outcome1,fixed=TRUE))
+        if(!any(state == outcome1$State)){
+                stop("invalid state")}
+        else if((outcome %in% c("heart attack", "heart failure",
+                                "pneumonia")) == FALSE) {
+                stop(print("invalid outcome"))
+        }
+        outcome2 <- subset(outcome1, State == state)
+        if (outcome == "heart attack") {
+                colnum <- 11
+        }
+        else if (outcome == "heart failure") {
+                colnum <- 17
+        }
+        else {
+                colnum <- 23
+        }
+        outcome2[ ,colnum] <- as.numeric(outcome2[ ,colnum])
+        outcome3 <- outcome2[order(outcome2[ ,colnum],outcome2[,2]), ]
+        outcome3 <- outcome3[(!is.na(outcome3[ ,colnum])),]
+        if(num == "best"){
+                num <- 1
+        }            
+        else if (num == "worst"){
+                num <- nrow(outcome3)
+        }      
+        return(outcome3[num,2])
+}
